e54550c45c750809bae6b632b2fcb14a39bda368
ENAFUL <enfulemondal91@gmail.com>
2026-06-04 11:28:58 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+export API_ID='33422392'
+export API_HASH='9b66ed5b9e6a307951b3a681c80f9d4b'
+export BOT_TOKEN='8943755391:AAGK66tioyQhT8kfF4TWJAPJAJfi-Z2Fiss'
+export SESSION_STRING='BQH9_DgAIu2fSPAKzNGvRK1oVgw9_G-AlwH2-T9_nu0MEb_dLHNUfVfmohLPC_0E9-RLeaQ5d2qmF2WkTrhO8v47mTE_D_7CGUGLX3JrpT0vvl_HQtGAa-yIWYsEm1Bcn-WDfercfIn2Sy3Lk9lv8l2Dzn1DQP2cUIVvF4fgJBtHAAmdMCPQfT8xIHfD4LSt8rWVQ4ozLdKQj6kyqGUkfZ5OdNuObuqExMrshxO_iyn8VG1TC530vl2Gt1UDtodbf8WwD8pRLAhHboMeF0X9Ut7XEPvLTOh4FQ79aVlbv463oi7R9idBz2PY_3C1bkPYwujvfRVNvtxRP58tPYYbkTNsr5_NJwAAAAHiAUodAA'
+export PATH='/root/.deno/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+
+# আপনার নতুন ফোল্ডার পাথে যাওয়া হচ্ছে
+cd /root/EnafulMusicBot
+
+# বটের ইউজারনেম ডিরেক্ট লিঙ্ক আকারে নিচে দেওয়া হলো
+# https://t.me/Enaful_Music69_Bot
+
+exec python3 -u bot.py
